423e48ebdf704f9d89d79958b791b8c5347fe599
Eduardo Espindola <Espindola.A.E@hotmail.com>
2024-04-22 14:57:42 -0600

    Odd_Numbers

--- 5-Understanding_Streams/4-Readable_Streams/readFile.js
+++ 5-Understanding_Streams/4-Readable_Streams/readFile.js
@@ -1,6 +1,6 @@
 const fs = require("node:fs/promises");
 
-const readFile = async (src, des) => {
+/* const readFile = async (src, des) => {
     try {
         const fileHandleRead = await fs.open(src, 'r');
         const fileHandleWrite = await fs.open(des, 'w');
@@ -22,6 +22,58 @@ const readFile = async (src, des) => {
         console.error(err);
     }
 };
-const src = './src.txt';
-const des = './des.txt';
+const src = './numbers.txt';
+const des = './numbersCopy.txt';
+readFile(src, des); */
+
+const readFile = async (src, des) => {
+    try {
+        console.time('Timer');
+        
+        const fileHandleRead = await fs.open(src, 'r');
+        const fileHandleWrite = await fs.open(des, 'w');
+        
+        const streamRead = fileHandleRead.createReadStream({highWaterMark: Math.pow(2, 20)});
+        const streamWrite = fileHandleWrite.createWriteStream({highWaterMark: Math.pow(2, 20)});
+        
+        let split = '';
+        
+        streamRead.on('data', (chunk) => {
+            const numbers = chunk.toString('utf-8').split(' ');
+            
+            if(Number( numbers[0]) !== Number(numbers[1]) -1) {
+                if(split) {
+                    numbers[0] = split.trim() + numbers[0].trim();
+                };
+            };
+            
+            if(Number(numbers[numbers.length -2]) +1 !== Number(numbers[numbers.length -1])) {
+                split = numbers.pop();
+            };
+            
+            numbers.forEach((number) => {
+                const n = Number(number);
+                if(n % 2 === 0){
+                    if(!streamWrite.write(`${n} `)){
+                        streamRead.pause();
+                    };
+                };
+            });
+        });
+        
+        streamWrite.on('drain', () => {
+            streamRead.resume();
+        });
+        
+        streamRead.on('end', () => {
+            console.log('Done reading.');
+            console.timeEnd('Timer');
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const src = './numbers.txt';
+const des = './oddNumbers.txt';
 readFile(src, des);
